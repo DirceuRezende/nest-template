cd24fca03e4e142959fe50b2180fdd3068fc4a7d
Dirceu Rezende <dirceu.srezende@gmail.com>
2022-02-22 20:06:40 -0300

    chore: add Prisma and Prisma Service

---
+++ prisma/migrations/20220222230152_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "verified_email" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
